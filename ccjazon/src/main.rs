9a14be5993b218ae2061fd40329a0027910f0cc2
Adriano Henrique de Carvalho Afonso e Silva <adriano.silva@brex.com>
2024-02-27 13:54:48 -0300

    Refactoring and adding Cli reading tool

--- ccjazon/src/main.rs
+++ ccjazon/src/main.rs
@@ -1,6 +1,22 @@
+use std::{fs, path::PathBuf};
+
 mod validator;
 mod parser;
 
+use clap::Parser;
+
+use crate::parser::tokenize;
+
+#[derive(Parser)]
+struct Cli {
+    path: std::path::PathBuf
+}
+
 fn main() {
-    println!("Hello world");
+    let args = Cli::parse();
+    let file_path = PathBuf::from(args.path);
+
+    let contents = fs::read_to_string(file_path).expect("Unable to read file");
+    let tokenized_contents = tokenize(contents);
+    println!("{:?}", tokenized_contents);
 }

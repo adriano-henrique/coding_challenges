e8dcbfff7359c642fd56b9fb43327581fe14f097
adriano-henrique <adrianohenriqueafonso@gmail.com>
2024-03-11 18:20:18 -0300

    Adding intial implementation for client server chat

--- ccchat/ccpychat/server.py
+++ ccchat/ccpychat/server.py
@@ -1,9 +1,20 @@
 import asyncio
 from websockets.server import serve
 
-async def echo(websocket):
-    i = 0
+CONNECTIONS = set()
+
+async def register(websocket):
+    global CONNECTIONS
+    CONNECTIONS.add(websocket)
+    try:
+        await websocket.wait_closed()
+    except:
+        CONNECTIONS.remove(websocket)
+
+async def chat_server(websocket):
+    print("New connection from " + websocket.remote_address[0] + ":" + str(websocket.remote_address[1]))
     name = await websocket.recv()
+    await register(websocket)
     print("Received name: " + name)
     async for message in websocket:
         print("Sending message: " + message)
@@ -11,7 +22,7 @@ async def echo(websocket):
 
 async def main():
     print('starting server')
-    async with serve(echo, 'localhost', 7007):
+    async with serve(chat_server, 'localhost', 7007):
         await asyncio.Future()  # run forever
 
 if __name__ == '__main__':
